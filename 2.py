eb748e245f4d8cbddabb6ca358c591aa64404d83
Leonardo <leonardorm7@hotmail.com>
2023-07-03 21:34:57 -0500

    exercise 2 finished, and finishing 3

---
+++ 2.py
@@ -0,0 +1,51 @@
+# Just imagine that we have already prepared lists of articles that are similar.  You can see an example below: 
+# Retrieve an article in PubMed: https://pubmed.ncbi.nlm.nih.gov/ 
+# Click on the desired record to display the Abstract view. 
+# Examine links to Related Articles, including Reviews, on the right side of the page, or click "See all" to display all related records. 
+
+# https://pubmed.ncbi.nlm.nih.gov/?linkname=pubmed_pubmed&from_uid=33400058 
+# you can see that: 
+# PMID: 33400058 (Recent Developments on Therapeutic and Diagnostic Approaches for COVID-19) 
+# has the following similar articles: 
+# 33400058: [32889088, 32935333, 33261606, 33781287, 33334802, 33070079, etc.] (623 results) 
+# https://pubmed.ncbi.nlm.nih.gov/?linkname=pubmed_pubmed&from_uid=33070079 
+# 33070079: [34276652, 33371468, etc.] 
+# . . .  
+# The task is to find all 2‑tuple (ordered pair or couple) for the given lists. For simplification just let’s have the following: 
+# similar_ids = { 
+#     123: [458, 812, 765], 
+#     458: [123, 812, 765], 
+#     812: [123, 458], 
+#     765: [123, 458], 
+#     999: [100], 
+#     100: [999] 
+# }
+# The expected result will be: 
+# expected = { 
+#     (123, 458), (123, 812), (123, 765), (458, 812), (458, 765), (100, 999) 
+# }
+# The tuples are ordered and appear only once, so if you have (123, 458) the (458, 123) is not possible. 
+# Please write a program on Python. You can put a link to your code (https://pastebin.com/ for example or a link to GitHub, GitLab, etc.).
+# The scripts which are not working wouldn't be evaluated. Any comments are also welcome.
+# The extra points will be given if you write tests for different input and outputs and if you follow PEP8.
+
+def similar_articles(similar_ids):
+    pairs=set()
+    for article, similar in similar_ids.items():
+        for similar_article in similar:
+            couple=tuple(sorted((article, similar_article)))
+            pairs.add(couple)
+    return pairs
+
+
+
+similar_ids = {
+    123: [458, 812, 765],
+    458: [123, 812, 765],
+    812: [123, 458],
+    765: [123, 458],
+    999: [100],
+    100: [999]
+}
+print(similar_articles(similar_ids))
+
---
+++ 3.py
@@ -0,0 +1,85 @@
+# Create a program that collects articles from the https://www.bbc.com/news website. We are only interested in the sections "/business" (Business - without the "Features & Analysis", "Watch/Listen" and "Special reports" subsections) and "/technology" (Tech - without the Watch/Listen and Features & Analysis subsections).
+
+# A. The script should save every article to a separate JSON file, keeping only the article content (TITLE and BODY). All non-relevant content (external links, links to categories, CSS, scripts, multimedia content, etc.) should be removed.
+
+# B. Make sure to keep track of the downloaded content. If run again, the script should only collect articles we have not downloaded already.
+
+# C. (bonus) Containerize your Python application. Outputs should be outside the container.
+
+# The program should be written in Python and could use the Selenium framework.
+# In order to keep formatting, please encode your script in Base64 and paste it bellow or you can put a link to your code (https://pastebin.com/ for example or a link to GitHub, GitLab, etc.)
+# The scripts which are not working wouldn't be evaluated. Any comments are also welcome.
+# Writing a code following PEP 8 is highly recommended.
+
+
+import os
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+OUTPUT_DIR = "articles"
+SECTIONS = ["business", "technology"]
+EXCLUDED_SUBSECTIONS = ["features", "watch", "listen", "special-reports"]
+
+
+def initialize_driver():
+    options = Options()
+    options.add_argument("--headless")  
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
+
+def save_json_article(title,body,output_file):
+    article={
+        "title":title,
+        "body":body
+    }
+    with open(output_file,"w",encoding="utf8") as file:
+        json.dump(article,file, ensure_ascii=False, indent=4)
+    print(f'Saved article: {title}')
+
+
+def scrape_articles(SECTIONS):
+    driver=initialize_driver()
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+    for section in SECTIONS:
+        url=f'https://www.bbc.com/news/{section}'
+        driver.get(url)
+        section_dir = os.path.join(OUTPUT_DIR, section)
+        if not os.path.exists(section_dir):
+            os.makedirs(section_dir)
+        if section == "business":
+            elements = driver.find_elements(By.XPATH, '//a[h3[contains(@class,"gs-c-promo-heading")] and not(ancestor::div[@role="region" and (@aria-labelledby="nw-c-Specialreports__title" or @aria-labelledby="nw-c-Watch/Listen__title" or @aria-labelledby="nw-c-Features&Analysis__title")]) and not(ancestor::nav)]')
+            links = [element.get_attribute('href') for element in elements]
+        if section == "technology":
+            elements = driver.find_elements(By.XPATH, '//a[h3[contains(@class,"gs-c-promo-heading")] and not(ancestor::div[@role="region" and (@aria-labelledby="nw-c-Watch/Listen__title" or @aria-labelledby="nw-c-Features&Analysis__title")]) and not(ancestor::nav)]')
+            links = [element.get_attribute('href') for element in elements]
+        # article_links = driver.find_elements(By.XPATH, f'//a[h3[@class="gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text"] and not(div[contains(@class,"nw-c-watchlisten")]) and not(div[contains(@class,"nw-c-features-analysis")]) and not(div[contains(@class,"nw-c-special-reports")])]')
+        # links = [element.get_attribute('href') for element in article_links]
+        print(" "*500)
+        print(links)
+        print(" "*500)
+        for link in links:
+            driver.get(link)
+            article_id = link.split("/")[-1]
+            print(" "*500)
+            print(link)
+            print(article_id)
+            print(" "*500)
+            title_element = driver.find_element(By.XPATH, '//h1[@class="article-headline__text b-reith-sans-font b-font-weight-300"] | //h1[@id="main-heading"]')
+            title = title_element.text
+            body_elements = driver.find_elements(By.XPATH, '//div[@data-component="text-block"]//p | //div[@class="article__body-content"]')
+            body = '\n'.join([element.text for element in body_elements])
+            
+            output_file=os.path.join(section_dir,f"{article_id}.json")
+            save_json_article(title,body,output_file)
+
+    driver.quit()
+if __name__=="__main__":
+    SECTIONS = ["business", "technology"]
+    scrape_articles(SECTIONS)
